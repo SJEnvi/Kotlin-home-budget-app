10ef0bedc9522f1ca8e8f593840508ac6d348131
Szymon Jankowski <63104347+SJEnvi@users.noreply.github.com>
2023-03-23 00:44:25 +0100

    Testing 3rd database provider
    Changed the authentication to Firebase which will be alter used to connect to firestore database

---
+++ src/main/java/com/example/myapplication/Arrays.kt
@@ -0,0 +1,27 @@
+//package com.example.myapplication
+//
+//fun main(){
+//
+//    //val numbers:IntArray = intArrayOf(1,2,3,5,4,3,1,2)
+//
+//    //val numbers = intArrayOf(1,2,3,5,4,3,1,2)
+//
+//    val numbers = arrayOf(1,2,3,5,4,3,1,2)
+//
+//    println(numbers.contentToString())
+//    for (element in numbers){
+//        print(element)
+//    }
+//
+//    val fruits = arrayOf(Fruit("Apple", 2.5), Fruit("Pear", 50.5), Fruit("Banana", 10.2))
+//
+//    for (index in fruits.indices){
+//        print("\n${fruits[index].name} is in index $index")
+//    }
+//
+//    for (fruit in fruits){
+//        println(fruit.name)
+//    }
+//}
+//
+//data class Fruit(val name: String, val price: Double)
